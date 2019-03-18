d3aefebefe7be1c1e5b01b0ae5e277e0dd44f970
Osito <Osito@DESKTOP-V1MVDT8>
2019-03-18 22:23:12 +0100

    dodano panel diagnostyka

--- admin072017_backup.php
+++ admin072017_backup.php
@@ -6,6 +6,14 @@
     if ($_SESSION['uczestnik']['uprawnienia'] != "admin") {
         die("Nie jesteś upoważniony do przeglądania zasobu");
     }
+    require_once($_SERVER['DOCUMENT_ROOT'].'/resources/php/Rb.php');
+    R::setup($_SESSION['host'].'dbname=p6273_odomg', 'p6273_odomg', 'P3rsKy_K@tek1');
+    $_coszukac = "SELECT * FROM `uczestnicy` WHERE datanadania IS NOT NULL AND datanadania <>'' AND wyslanymailupr=0";
+    $_uzytkownicybezlinka = R::getAll($_coszukac);
+    $_coszukac = "SELECT * FROM `uczestnicy` WHERE sessionend IS NOT NULL AND wyslanycert=0";
+    $_uzytkownicybezzaswiadczenia = R::getAll($_coszukac);
+    $_coszukac = "SELECT * FROM `uczestnicy` WHERE datanadania IS NOT NULL AND datanadania <>'' AND utworzony IS NOT NULL AND wyslaneup=0";
+    $_uzytkownicybezupowaznienia = R::getAll($_coszukac);
     ?>
     <head>
         <meta charset="utf-8">
@@ -27,6 +35,15 @@
         <script src="/resources/js/potwierdzenia.js?v180118"></script>
         <script src="/resources/primeui-4.1.12/primeui.min.js?v180118"></script>
         <script src="/resources/js/admin_072017_backup.js?v180118"></script>
+        <link rel="stylesheet" href="/resources/dataTableNew/media/css/jquery.dataTables.css?v180118"/>
+         <link href="resources/dataTableNew/extensions/KeyTable/css/keyTable.dataTables.css?v180118" rel="stylesheet" type="text/css"/>
+        <link href="resources/dataTableNew/extensions/Select/css/select.dataTables.css?v180118" rel="stylesheet" type="text/css"/>
+        <link href="resources/dataTableNew/extensions/Buttons/css/buttons.dataTables.css?v180118" rel="stylesheet" type="text/css"/>
+        <script src="/resources/dataTableNew/media/js/jquery.dataTables.js?v180118"></script>
+        <script src="resources/jszip.min/jszip.min.js?v180118" type="text/javascript"></script>
+        <script src="resources/PDFMAKE_files/pdfmake.min.js?v180118" type="text/javascript"></script>
+        <script src="resources/PDFMAKE_files/vfs_fonts.js?v180118" type="text/javascript"></script>
+        <script src="resources/dataTableNew/extensions/Buttons/js/buttons.html5.min.js?v180118" type="text/javascript"></script>
         <title>Testy Dane Wrażliwe</title>
         <style>
             .center {
@@ -44,15 +61,100 @@
             include_once './menu_backup.php';
         ?>
         <div style="box-shadow: 10px 10px 5px #888; padding: 30px;  margin-top: 10px; background-color: gainsboro; ">
+            <div>
             <form >
-                <button id='potwierdz' type='button' value="archiwizuj" onclick="archiwizuj();" style="width: 160px; margin-top: 1%; margin-left: 3%;">archiwizuj dane</button>
+                <button id='potwierdz' type='button' value="archiwizuj" onclick="archiwizuj();" style="width: 160px; margin-top: 1%; ">archiwizuj dane</button>
             </form>
             <p id="pole1"  style="display: none; color: blue; margin-left: 3%; font-weight: 900;">Rozpoczynam archiwizację</p>
             <p id="pole2"  style="display: none; color: blue; margin-left: 3%; font-weight: 900;">Zakończyłem archiwizację</p>
+            </div>
+            <div style="width: 950px;margin-top: 25px;">
+                <p style="font-weight: bolder; color: blue;">Uczestnicy bez wysłanego linka</p>
+                <table  id="tab1" style="width: 1050px;">
+                        <thead>
+                            <tr >
+                                <th style="text-align: center">imię i nazwisko</th>
+                                <th style="text-align: center">email</th>
+                                <th style="text-align: center">firma</th>
+                                <th style="text-align: center">zaciągnięty</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                                 <?php 
+                                foreach ($_uzytkownicybezlinka as $value) { 
+                                ?>
+                                <tr>
+                                <td style="text-align: left"><?php echo $value[imienazwisko];?></td>
+                                <td style="text-align: left"><?php echo $value[email];?></td>
+                                <td style="text-align: left"><?php echo $value[firma];?></td>
+                                <td style="text-align: left"><?php echo $value[utworzony];?></td>
+                                </tr>
+                                <?php
+                                } ?>
+                        </tbody>
+                    </table>
+            </div>
+            <div style="width: 950px;margin-top: 25px;">
+                <p style="font-weight: bolder; color: blue;">Uczestnicy bez wysłanego zaświadczenia</p>
+                <table  id="tab2" style="width: 1050px;">
+                        <thead>
+                            <tr >
+                                <th style="text-align: center">imię i nazwisko</th>
+                                <th style="text-align: center">email</th>
+                                <th style="text-align: center">firma</th>
+                                <th style="text-align: center">zaciągnięty</th>
+                                <th style="text-align: center">test zakończony</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                                 <?php 
+                                foreach ($_uzytkownicybezzaswiadczenia as $value) { 
+                                ?>
+                                <tr>
+                                <td style="text-align: left"><?php echo $value[imienazwisko];?></td>
+                                <td style="text-align: left"><?php echo $value[email];?></td>
+                                <td style="text-align: left"><?php echo $value[firma];?></td>
+                                <td style="text-align: left"><?php echo $value[utworzony];?></td>
+                                <td style="text-align: left"><?php echo $value[sessionend];?></td>
+                                </tr>
+                                <?php
+                                } ?>
+                        </tbody>
+                    </table>
+            </div>
+            <div style="width: 950px;margin-top: 25px;">
+                <p style="font-weight: bolder; color: blue;">Uczestnicy bez wysłanego upoważnienia</p>
+                <table  id="tab3" style="width: 1050px;">
+                        <thead>
+                            <tr >
+                                <th style="text-align: center">imię i nazwisko</th>
+                                <th style="text-align: center">email</th>
+                                <th style="text-align: center">firma</th>
+                                <th style="text-align: center">zaciągnięty</th>
+                                <th style="text-align: center">data nadania</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                                 <?php 
+                                foreach ($_uzytkownicybezupowaznienia as $value) { 
+                                $nowadatanadania = explode(".", $value[datanadania]);
+                                $nowadatanadania2 = $nowadatanadania[2]."-".$nowadatanadania[1]."-".$nowadatanadania[0];
+                                ?>
+                                <tr>
+                                <td style="text-align: left"><?php echo $value[imienazwisko];?></td>
+                                <td style="text-align: left"><?php echo $value[email];?></td>
+                                <td style="text-align: left"><?php echo $value[firma];?></td>
+                                <td style="text-align: left"><?php echo $value[utworzony];?></td>
+                                <td style="text-align: left"><?php echo $nowadatanadania2;?></td>
+                                </tr>
+                                <?php
+                                } ?>
+                        </tbody>
+                    </table>
+            </div>
             <div id="ajax_sun" title="archiwizowanie" style="display: none; text-align: center;" class="center">;
-            <img src="/images/ajax_loaderc.gif" alt="ajax" height="70" width="70">;
-        </div>
-        
+                <img src="/images/ajax_loaderc.gif" alt="ajax" height="70" width="70">;
+            </div>
         </div>
     </body>
 </html>
--- resources/js/admin_072017_backup.js
+++ resources/js/admin_072017_backup.js
@@ -9,6 +9,80 @@ $(document).ready(function () {
     });
     podswietlmenu(rj('menubackup'));
     $("#potwierdz").puibutton();
+    var uTable = $('#tab1').dataTable(
+        {
+            "bJQueryUI": true,
+            "sPaginationType": "full_numbers",
+            "keys": true,
+            "select": "single",
+            "language": {
+                "url": "resources/dataTableNew/Polish.json"
+            },
+            "dom": 'lfrBtip',
+            "buttons": [
+                'copyHtml5',
+                'excelHtml5',
+                'csvHtml5',
+                {
+                    extend: 'pdfHtml5',
+                    orientation: 'landscape',
+                    pageSize: 'A4'
+                }
+            ]
+        }
+    );
+    $('#tab1').addClass("compact");
+    uTable.fnSort([[3, 'desc']]);
+    var uTable1 = $('#tab2').dataTable(
+        {
+            "bJQueryUI": true,
+            "sPaginationType": "full_numbers",
+            "keys": true,
+            "select": "single",
+            "language": {
+                "url": "resources/dataTableNew/Polish.json"
+            },
+            "dom": 'lfrBtip',
+            "buttons": [
+                'copyHtml5',
+                'excelHtml5',
+                'csvHtml5',
+                {
+                    extend: 'pdfHtml5',
+                    orientation: 'landscape',
+                    pageSize: 'A4'
+                }
+            ]
+        }
+    );
+    $('#tab2').addClass("compact");
+    uTable1.fnSort([[1, 'asc']]); 
+    $('#tab1').addClass("compact");
+    uTable.fnSort([[3, 'desc']]); 
+    var uTable2 = $('#tab3').dataTable(
+        {
+            "bJQueryUI": true,
+            "sPaginationType": "full_numbers",
+            "keys": true,
+            "select": "single",
+            "language": {
+                "url": "resources/dataTableNew/Polish.json"
+            },
+            "dom": 'lfrBtip',
+            "buttons": [
+                'copyHtml5',
+                'excelHtml5',
+                'csvHtml5',
+                {
+                    extend: 'pdfHtml5',
+                    orientation: 'landscape',
+                    pageSize: 'A4'
+                }
+            ]
+        }
+    );
+    $('#tab3').addClass("compact");
+    uTable2.fnSort([[4, 'desc']]); 
 });
 var archiwizuj = function () {
     $("#pole1").show();
